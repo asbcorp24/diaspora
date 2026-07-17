4a213b783d6c349e9b46d87ef234aa335506bc9d
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-17 17:15:33 +0300

    Rename admin legal templates section

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('admin.index', function ($view): void {
+            $sections = $view->getData()['sections'] ?? [];
+            if (isset($sections['letters'])) {
+                $sections['letters'] = 'Юридическая помощь';
+            }
+            $view->with('sections', $sections);
+        });
     }
 }
