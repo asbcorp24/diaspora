834237be50a82888982425b80320105267f3f526
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-17 14:11:16 +0300

    Register employer and rental review routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PlatformController;
+use App\Http\Controllers\ReviewController;
 use App\Http\Middleware\ResolveDiaspora;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,8 @@ Route::middleware(ResolveDiaspora::class)->group(function (): void {
     Route::get('/community', [PlatformController::class, 'community'])->name('community');
     Route::post('/community/posts', [PlatformController::class, 'storePost'])->middleware(['auth', 'throttle:30,1'])->name('posts.store');
 
+    Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews');
+
     Route::middleware('auth')->group(function (): void {
         Route::get('/messages', [PlatformController::class, 'messages'])->name('messages');
         Route::post('/messages/start/{user}', [PlatformController::class, 'startConversation'])->name('messages.start');
@@ -26,6 +29,19 @@ Route::middleware(ResolveDiaspora::class)->group(function (): void {
         Route::post('/messages/{conversation}', [PlatformController::class, 'sendMessage'])->where('conversation', '[0-9]+')->middleware('throttle:60,1')->name('messages.send');
         Route::post('/jobs', [PlatformController::class, 'storeJob'])->middleware('throttle:10,1')->name('jobs.store');
         Route::post('/safety/report', [PlatformController::class, 'reportIncident'])->middleware('throttle:5,10')->name('safety.report');
+
+        Route::post('/reviews/employers', [ReviewController::class, 'storeEmployer'])->middleware('throttle:5,10')->name('reviews.employer.store');
+        Route::post('/reviews/rentals', [ReviewController::class, 'storeRental'])->middleware('throttle:5,10')->name('reviews.rental.store');
+        Route::post('/reviews/{type}/{review}/report', [ReviewController::class, 'report'])
+            ->where('type', 'employer|rental')
+            ->where('review', '[0-9]+')
+            ->middleware('throttle:10,10')
+            ->name('reviews.report');
+        Route::get('/reviews-moderation', [ReviewController::class, 'moderation'])->name('reviews.moderation');
+        Route::post('/reviews-moderation/{type}/{review}', [ReviewController::class, 'moderate'])
+            ->where('type', 'employer|rental')
+            ->where('review', '[0-9]+')
+            ->name('reviews.moderate');
     });
 
     Route::get('/jobs', [PlatformController::class, 'jobs'])->name('jobs');
